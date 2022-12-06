13eee5fe4944ee64bd8a57c4b5cddb1a30fc01f2
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-06 16:31:20 +0700

    add: endpoint login

---
+++ route/route.go
@@ -0,0 +1,19 @@
+package route
+
+import (
+	"go-docker/handler"
+	"go-docker/user"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func Router(db *gorm.DB, r *gin.Engine) {
+	// user
+	userRepo := user.NewRepository(db)
+	userService := user.NewService(userRepo)
+	userHandler := handler.NewHandlerUser(userService)
+
+	v1 := r.Group("v1")
+	v1.POST("/login", userHandler.Login)
+}
