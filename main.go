f7920878113bb2ddea155ea72363315b1f55a000
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-06 16:31:52 +0700

    update: configuratioin

--- main.go
+++ main.go
@@ -1,41 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"go-docker/db"
+	"go-docker/route"
 	"log"
-	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	env, err := godotenv.Read(".env")
+	dbConnection, err := db.Connection(".env")
 	if err != nil {
-		log.Fatalf("ERROR : %v", err)
+		log.Fatalf("ERROR CONNECTION : %v", err)
 	}
-
-	// env author akan di set melalu dockerfile
-	// karena dockerfile juga bisa mengartur value env
-	author := os.Getenv("AUTHOR")
-	port := env["PORT"]
-	address := fmt.Sprintf("0.0.0.0:%v", port)
 
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		response := "Hallo "
-		if author != "" {
-			response = response + author
-		}
+	route.Router(dbConnection, r)
 
-		c.JSON(http.StatusOK, map[string]string{
-			"message": response,
-		})
-	})
-
-	log.Println("server run on : ", address)
-
-	r.Run(address)
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
