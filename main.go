dda6ab3babd3a7083d9828254c05b22c7e0ef4fa
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-09 10:23:27 +0700

    add: migration and move env

--- main.go
+++ main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
-	"go-docker/db"
+	"fmt"
 	"go-docker/route"
 	"log"
+	"os/user"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	dbConnection, err := db.Connection(".env")
+
+	env, err := godotenv.Read(".env.prod")
 	if err != nil {
-		log.Fatalf("ERROR CONNECTION : %v", err)
+		errMsg := fmt.Sprintf("ERROR GET ENV : %v", err)
+		log.Fatal(errMsg)
+		return
 	}
+
+	databaseUsername := env["DATABASE_USERNAME"]
+	databasePassword := env["DATABASE_PASSWORD"]
+	databaseHost := env["DATABASE_HOST"]
+	databasePort := env["DATABASE_PORT"]
+	databaseName := env["DATABASE_NAME"]
+
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", databaseUsername, databasePassword, databaseHost, databasePort, databaseName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		errMsg := fmt.Sprintf("ERROR CONNECTION : %v", err)
+		log.Fatal(errMsg)
+		return
+	}
+
+	db.AutoMigrate(&user.User{})
 
 	r := gin.Default()
 
-	route.Router(dbConnection, r)
+	route.Router(db, r)
 
-	if err := r.Run(":8080"); err != nil {
+	if err := r.Run("0.0.0.0:8080"); err != nil {
 		log.Fatal(err)
 	}
 }
